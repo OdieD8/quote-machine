a9ce9579cca052e2416e6dec3fd53765507c6b66
OdieD8 <odie_de_moura@hotmail.com>
2018-03-02 18:09:23 -0700

    Working on quote service

--- public/features/home/home.controller.js
+++ public/features/home/home.controller.js
@@ -1,4 +1,13 @@
 angular.module("app").controller("homeController", function($scope, $state, userService) {
 	
+	$scope.newQuote = function() {
+		
+		userService.getQuote().then(function(data) {
+			
+			$scope.quote = data.data[0].content;
+			$scope.author = data.data[0].title;
+		});
+	}
 	
+	//$scope.newQuote();
 })
--- public/services/user.service.js
+++ public/services/user.service.js
@@ -1,4 +1,16 @@
-angular.module("app").service("userService", function($state) {
+angular.module("app").service("userService", function($state, $q, $http) {
 	
-	
+	this.getQuote = function() {
+		
+			var quoteApi = {
+			endpoint: 'https://quotesondesign.com/wp-json/posts',
+			params: {
+				'filter[orderby]': 'rand',
+				'filter[posts_per_page]': 1,
+				'timestamp': new Date().getTime()
+        	}
+    	};
+         
+    	return $http.get(quoteApi.endpoint, { params: quoteApi.params } );
+	}
 })
